93af7c34d22d2a2d60da25438a4b121bc403a97c
Manuel Silva <msilva28.dev@gmail.com>
2016-06-13 02:50:28 -0700

    Added permissions check for photo access and access to the gallery.

--- app/src/main/java/meta/simplifi/dropboxuploader/MainActivity.java
+++ app/src/main/java/meta/simplifi/dropboxuploader/MainActivity.java
@@ -1,8 +1,13 @@
 package meta.simplifi.dropboxuploader;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -18,23 +23,29 @@ import android.view.View;
 import com.dropbox.core.android.Auth;
 
 import meta.simplifi.dropboxuploader.databinding.ActivityMainBinding;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     public static final String AUTH_TOKEN = "auth-token";
-    private SharedPreferences mPreferences;
     private String mToken;
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onResume() {
         super.onResume();
-        mPreferences = getSharedPreferences(AUTH_TOKEN, MODE_PRIVATE);
-        mToken = mPreferences.getString(AUTH_TOKEN, null);
+        SharedPreferences preferences = getSharedPreferences(AUTH_TOKEN, MODE_PRIVATE);
+        mToken = preferences.getString(AUTH_TOKEN, null);
         if (mToken == null) {
             mToken = Auth.getOAuth2Token();
             if (mToken != null) {
-                mPreferences.edit().putString(AUTH_TOKEN, mToken).apply();
+                preferences.edit().putString(AUTH_TOKEN, mToken).apply();
             }
         }
     }
@@ -42,38 +53,79 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        Toolbar toolbar = binding.appBarMain.toolbar;
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Toolbar toolbar = mBinding.appBarMain.toolbar;
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = binding.appBarMain.fab;
+        FloatingActionButton fab = mBinding.appBarMain.fab;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!hasToken()) {
-                    Snackbar.make(view, "You have to log in to upload pictures", Snackbar.LENGTH_LONG)
+                    Snackbar.make(view, "Log in to Dropbox to upload pictures", Snackbar.LENGTH_LONG)
                             .setAction("Go", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
                                     Auth.startOAuth2Authentication(MainActivity.this, getString(R.string.app_key));
                                 }
                             }).show();
+                } else {
+                    MainActivityPermissionsDispatcher.openGalleryWithCheck(MainActivity.this);
                 }
             }
         });
 
-        DrawerLayout drawer = binding.drawerLayout;
+        DrawerLayout drawer = mBinding.drawerLayout;
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = binding.navView;
+        NavigationView navigationView = mBinding.navView;
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     private boolean hasToken() {
         return mToken != null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Choose a picture to upload"), 0);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @OnShowRationale(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void onShowStorageRationale(PermissionRequest request) {
+        showPermissionSnackbar();
+        request.proceed();
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @OnNeverAskAgain(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void onNeverAskAgain() {
+        showPermissionSnackbar();
+    }
+
+    private void showPermissionSnackbar() {
+        Snackbar.make(
+                mBinding.appBarMain.fab,
+                "We need storage permissions to upload pictures",
+                Snackbar.LENGTH_INDEFINITE
+        ).setAction(
+                getString(android.R.string.ok),
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MainActivityPermissionsDispatcher
+                                .openGalleryWithCheck(MainActivity.this);
+                    }
+                }
+        ).show();
     }
 
     @Override
@@ -131,5 +183,11 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 }
